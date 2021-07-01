2821adfa6b59bc64b9f999bd4f7dfc8f954f48af
Lee Newberg <lee.newberg@kitware.com>
2021-07-01 10:51:37 -0400

    STYLE: clang-format and black formatting

--- src/itkVkCommon.cxx
+++ src/itkVkCommon.cxx
@@ -69,9 +69,9 @@ VkCommon::ConfigureBackend()
     std::cerr << __FILE__ "(" << __LINE__ << "): clGetPlatformIDs returned " << resCL << std::endl;
     return VKFFT_ERROR_FAILED_TO_INITIALIZE;
   }
-  // std::unique_ptr<cl_platform_id[]> platformsArray{ std::make_unique<cl_platform_id[]>(numPlatforms) }; // C++14!!!
-  std::unique_ptr<cl_platform_id[]> platformsArray{ new cl_platform_id[numPlatforms] };
-  cl_platform_id *                  platforms{ &platformsArray[0] };
+  std::unique_ptr<cl_platform_id[]> platformsArray{ std::make_unique<cl_platform_id[]>(numPlatforms) };
+  // std::unique_ptr<cl_platform_id[]> platformsArray{ new cl_platform_id[numPlatforms] };
+  cl_platform_id * platforms{ &platformsArray[0] };
   if (!platforms)
     return VKFFT_ERROR_MALLOC_FAILED;
   resCL = clGetPlatformIDs(numPlatforms, platforms, nullptr);
@@ -85,9 +85,9 @@ VkCommon::ConfigureBackend()
   {
     cl_uint numDevices;
     resCL = clGetDeviceIDs(platforms[j], CL_DEVICE_TYPE_ALL, 0, nullptr, &numDevices);
-    // std::unique_ptr<cl_device_id[]> deviceListArray{ std::make_unique<cl_device_id[]>(numDevices) }; // C++14!!!
-    std::unique_ptr<cl_device_id[]> deviceListArray{ new cl_device_id[numDevices] };
-    cl_device_id *                  deviceList{ &deviceListArray[0] };
+    std::unique_ptr<cl_device_id[]> deviceListArray{ std::make_unique<cl_device_id[]>(numDevices) };
+    // std::unique_ptr<cl_device_id[]> deviceListArray{ new cl_device_id[numDevices] };
+    cl_device_id * deviceList{ &deviceListArray[0] };
     if (!deviceList)
       return VKFFT_ERROR_MALLOC_FAILED;
     resCL = clGetDeviceIDs(platforms[j], CL_DEVICE_TYPE_ALL, numDevices, deviceList, nullptr);
