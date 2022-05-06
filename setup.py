4e12c46cdea709c7769bfa94081498092906a965
Tom Birdsong <40648863+tbirdso@users.noreply.github.com>
2022-05-06 17:17:49 -0400

    Bump to version v0.1.6
    Adds `itk.VkDiscreteGaussianImageFilter` for blurring speedup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name="itk-vkfft",
-    version="0.1.5",
+    version="0.1.6",
     author="Insight Software Consortium",
     author_email="itk+community@discourse.itk.org",
     packages=["itk"],
