4c968f16b0704568796c162457edcb3318a16235
Tom Birdsong <tom.birdsong@kitware.com>
2022-10-11 15:02:48 -0400

    ENH: Bump to v5.3rc04.post3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name="itk-vkfft",
-    version="0.1.7",
+    version="0.1.8",
     author="Insight Software Consortium",
     author_email="itk+community@discourse.itk.org",
     packages=["itk"],
@@ -42,5 +42,5 @@ setup(
     license="Apache",
     keywords="ITK InsightToolkit",
     url=r"https://itk.org/",
-    install_requires=[r"itk>=5.3rc04.post2"],
+    install_requires=[r"itk>=5.3rc04.post3"],
 )
