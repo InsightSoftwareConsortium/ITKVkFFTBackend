eae3ddd01c9f0aa5380eb801d56746bb6f529fda
Tom Birdsong <tom.birdsong@kitware.com>
2022-10-20 14:52:43 -0400

    ENH: Support ITK tarball specializations in linux build

--- wrapping/dockcross-manylinux-download-cache.sh
+++ wrapping/dockcross-manylinux-download-cache.sh
@@ -7,10 +7,12 @@ curl https://data.kitware.com/api/v1/file/592dd8068d777f16d01e1a92/download -o z
 gunzip -d zstd-1.2.0-linux.tar.gz
 tar xf zstd-1.2.0-linux.tar
 
-curl -L https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/${ITK_PACKAGE_VERSION:=v5.3rc04.post2}/ITKPythonBuilds-linux.tar.zst -O
-./zstd-1.2.0-linux/bin/unzstd ITKPythonBuilds-linux.tar.zst -o ITKPythonBuilds-linux.tar
+TARBALL_NAME="ITKPythonBuilds-linux${TARBALL_SPECIALIZATION}.tar"
+curl -L https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/${ITK_PACKAGE_VERSION:=v5.3rc04.post2}/${TARBALL_NAME}.zst -O
+./zstd-1.2.0-linux/bin/unzstd ${TARBALL_NAME}.zst -o ${TARBALL_NAME}
 echo "Extracting all files"
-tar xf ITKPythonBuilds-linux.tar
+tar xf ${TARBALL_NAME}
+rm ${TARBALL_NAME}
 
 mkdir tools
 curl https://data.kitware.com/api/v1/file/5c0aa4b18d777f2179dd0a71/download -o doxygen-1.8.11.linux.bin.tar.gz
